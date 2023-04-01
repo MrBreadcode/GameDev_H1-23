658a56fbaab07ff0b0d247e1430e2e29139e99cc
MrBreadcode <76493650+MrBreadcode@users.noreply.github.com>
2023-04-01 22:42:59 +0300

    Add files via upload

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Player{
+namespace Player
+{
+
     [RequireComponent(typeof(Rigidbody2D))]
 
     public class PlayerEntity : MonoBehaviour
     {
+        //[SerializeField] private Animator _animator;
+
         [Header("HorizontalMovement")]
         [SerializeField] private float _horizontalSpeed;
         [SerializeField] private bool _faceRight;
+
+        [Header("VerticalMovement")]
+        [SerializeField] private float _verticalSpeed;
+        [SerializeField] private float _minSize;
+        [SerializeField] private float _maxSize;
+        [SerializeField] private float _maxVerticalPosition;
+        [SerializeField] private float _minVerticalPosition;
 
         [Header("Jump")]
         [SerializeField] private float _jumpForce;
@@ -21,23 +32,59 @@ namespace Player{
         private bool _isJumping;
         private float _startJumpVerticalPosition;
 
+        /*private Vector2 _movement;
+        private AnimationType _currentAnimationType*/
+
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-        }
 
+            float positionDifference = _maxVerticalPosition - _minVerticalPosition;
+            float sizeDifference = _maxSize - _minSize;
+            _sizeModificator = sizeDifference / positionDifference;
+            UpdateSize();
+        }
+        
         private void Update()
         {
             if (_isJumping)
                 UpdateJump();
-        }
 
+            //UpdateAnimation();
+        }
+        /*
+        private void UpdateAnimation()
+        {
+            PlayAnimation(AnimationType.Idle, true);
+            PlayAnimation(AnimationType.Go, _movement.magnitude > 0);
+            PlayAnimation(AnimationType.Jump, _isJumping);
+        }
+        */
         public void MoveHorizontally(float direction)
         {
+            //_movement.x = direction; 
             SetDirection(direction);
             Vector2 velocity = _rigidbody.velocity;
             velocity.x = direction * _horizontalSpeed;
             _rigidbody.velocity = velocity;
+        }
+
+        public void MoveVertically(float direction)
+        {
+            if (_isJumping)
+                return;
+
+            Vector2 velocity = _rigidbody.velocity;
+            velocity.y = direction * _verticalSpeed;
+            _rigidbody.velocity = velocity;
+
+            if (direction == 0)
+                return;
+
+            float verticalPosition = Mathf.Clamp(transform.position.y, _minVerticalPosition, _maxVerticalPosition);
+            _rigidbody.position = new Vector2(_rigidbody.position.x, verticalPosition);
+
+            UpdateSize();
         }
         
         public void Jump()
@@ -50,11 +97,20 @@ namespace Player{
             _startJumpVerticalPosition = transform.position.y;
         }
 
+        private void UpdateSize()
+        {
+            float verticalDelta = _maxVerticalPosition - transform.position.y;
+            float currentSizeModificator = _minSize + _sizeModificator * verticalDelta;
+            transform.localScale = Vector2.one * currentSizeModificator;
+        }
 
         private void SetDirection(float direction)
         {
-            if ((_faceRight && direction < 0) || (!_faceRight && direction > 0))
+            if ((_faceRight && direction < 0) || 
+                (!_faceRight && direction > 0))
                 Flip();
+
+            float verticalDelta = _maxVerticalPosition - transform.position.y;
         }
         
         private void Flip()
@@ -62,7 +118,7 @@ namespace Player{
             transform.Rotate(0, 180, 0);
             _faceRight = !_faceRight;
         }
-
+        
         private void UpdateJump()
         {
             if(_rigidbody.velocity.y < 0 && _rigidbody.position.y <= _startJumpVerticalPosition)
@@ -71,13 +127,36 @@ namespace Player{
                 return;
             }
         }
-
+       
         private void ResetJump()
         {
             _isJumping = false;
             _rigidbody.position = new Vector2(_rigidbody.position.x, _startJumpVerticalPosition);
             _rigidbody.gravityScale = 0;
         }
+        /*private void PlayAnimation(AnimationType animationType, bool active)
+        {
+            if (!active)
+            {
+                if (_currentAnimationType == animationType.Idle || _currentAnimationType != animationType)
+                    return;
+                _currentAnimationType = animationType.Idle;
+                PlayAnimation(_currentAnimationType);
+                return;
+
+            }
+
+            if(_currentAnimationType >= animationType)
+                return;
+
+            _currentAnimationType = animationType;
+            PlayAnimation(_currentAnimationType);
+        }
+
+        private void PlayAnimation(AnimationType animationType)
+        {
+            _animator.SetInteger(nameof(AnimationType), (int)animationType);
+        }*/
     }
 }
 
