9bd121accf998982ab5e0e151dacc5b29d6a20c9
MrBreadcode <76493650+MrBreadcode@users.noreply.github.com>
2023-04-12 23:58:58 +0300

    Add files via upload

---
+++ Assets/Scripts/Core/Movement/Data/JumpData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Player;
+using Core.Movement.Controller;
+using Core.Movement.Data;
+using Core.Enums;
+using Core.Tools;
+
+namespace Core.Movement.Data
+{
+    [Serializable]
+    public class JumpData
+    {
+        [field: SerializeField] public float JumpForce { get; private set; }
+        [field: SerializeField] public int GravityScale { get; private set; }
+    }
+}
